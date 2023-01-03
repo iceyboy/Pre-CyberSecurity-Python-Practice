715c79158fc04bd712330ae91a8060d09f7e8ed9
Ryan King <ryan.king2016@loretomilford.net>
2023-01-03 00:08:40 +0000

    Add files via upload

---
+++ practical_in_python_1.py
@@ -0,0 +1,4 @@
+print("Hello world! I am alive. \n \nI can write on two lines!\n")
+print("Happy Christmas \n\t*\n       ***\n      *****\n     *******\n    *********\n")
+print("First Witch\t When shall we three meet again\n\t\t In thunder, lightning, or in rain?\n\nSecond Witch\t When the hurlyburly's done,\n\t\t When the battle's lost and won.\n\nThird Witch\t That will be ere the set of sun.\n")
+
