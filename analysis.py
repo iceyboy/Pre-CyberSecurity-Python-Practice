a985e714cfebd0f0fc0e285d547eb354c0482688
Ryan King <ryan.king2016@loretomilford.net>
2023-02-11 13:45:38 +0000

    Add files via upload

---
+++ analysis.py
@@ -0,0 +1,23 @@
+def statistics(ciphertext):
+    list_stat = [0 for x in range(26)]  # A liste with  0s
+    for letter in ciphertext:  # go through text
+        i = ord(letter) - 65
+        if 0 <= i < 26:  # make sure it's a letter
+            list_stat[i] = list_stat[i] + 1
+    return (list_stat)
+
+
+# print and sort
+
+def statistics_sort(ciphertext):
+    list = statistics(ciphertext)
+    sortlist = []  # empty List
+    for i in range(26):
+        sortlist.append((chr(65 + i), list[i]))
+    tri = sorted(sortlist, key=lambda freq: freq[1], reverse=True)
+    return (tri)
+
+
+ciphertext = "GFS WMY OG LGDVS MF SFNKYHOSU ESLLMRS, PC WS BFGW POL DMFRQMRS, PL OG CPFU M UPCCSKSFO HDMPFOSXO GC OIS LMES DMFRQMRS DGFR SFGQRI OG CPDD GFS LISSO GK LG, MFU OISF WS NGQFO OIS GNNQKKSFNSL GC SMNI DSOOSK. WS NMDD OIS EGLO CKSJQSFODY GNNQKKPFR DSOOSK OIS 'CPKLO', OIS FSXO EGLO GNNQKKPFR DSOOSK OIS 'LSNGFU' OIS CGDDGWPFR EGLO GNNQKKPFR DSOOSK OIS 'OIPKU', MFU LG GF, QFOPD WS MNNGQFO CGK MDD OIS UPCCSKSFO DSOOSKL PF OIS HDMPFOSXO LMEHDS. OISF WS DGGB MO OIS NPHISK OSXO WS WMFO OG LGDVS MFU WS MDLG NDMLLPCY POL LYEAGDL. WS CPFU OIS EGLO GNNQKKPFR LYEAGD MFU NIMFRS PO OG OIS CGKE GC OIS 'CPKLO' DSOOSK GC OIS HDMPFOSXO LMEHDS, OIS FSXO EGLO NGEEGF LYEAGD PL NIMFRSU OG OIS CGKE GC OIS 'LSNGFU' DSOOSK, MFU OIS CGDDGWPFR EGLO NGEEGF LYEAGD PL NIMFRSU OG OIS CGKE GC OIS 'OIPKU' DSOOSK, MFU LG GF, QFOPD WS MNNGQFO CGK MDD LYEAGDL GC OIS NKYHOGRKME WS WMFO OG LGDVS"
+print(statistics(ciphertext))
+print(statistics_sort(ciphertext))
